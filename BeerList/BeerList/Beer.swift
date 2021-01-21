3b8d910f28ca1c07f4d9eeb3548a7352fab6c064
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-21 20:56:09 -0300

    Create Beer model

---
+++ BeerList/BeerList/Beer.swift
@@ -0,0 +1,18 @@
+//
+//  Beer.swift
+//  BeerList
+//
+//  Created by Tulio Parreiras on 21/01/21.
+//
+
+import Foundation
+
+struct Beer {
+    let id: Int
+    let name: String
+    let tagline: String
+    let description: String
+    let imageURL: URL
+    let abv: Double
+    let ibu: Double
+}
