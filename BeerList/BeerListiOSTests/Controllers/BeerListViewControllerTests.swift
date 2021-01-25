fe1d8654dc4e34c2d0898c2d131c168df3e5962c
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-25 12:48:53 -0300

    Move BeerListViewController test helpers to a separate file

--- BeerList/BeerListiOSTests/Controllers/BeerListViewControllerTests.swift
+++ BeerList/BeerListiOSTests/Controllers/BeerListViewControllerTests.swift
@@ -330,59 +330,6 @@ class BeerListViewControllerTests: XCTestCase {
 
 }
 
-private extension BeerListViewController {
-    
-    func simulateUserInitiatedBeerListReload() {
-        refreshControl?.simulateEvent(.valueChanged)
-    }
-    
-    @discardableResult
-    func simulateBeerCellVisible(at row: Int) -> BeerCell? {
-        return beerCell(at: row) as? BeerCell
-    }
-    
-    func simulateBeerCellNotVisible(at row: Int) {
-        let view = simulateBeerCellVisible(at: row)
-        
-        let delegate = tableView.delegate
-        let index = IndexPath(row: row, section: beerCellsSection)
-        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-    }
-    
-    func simulateBeerCellNearVisible(at row: Int) {
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: beerCellsSection)
-        ds?.tableView(tableView, prefetchRowsAt: [index])
-    }
-    
-    func simulateBeerCellNotNearVisible(at row: Int) {
-        simulateBeerCellNearVisible(at: row)
-        
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: beerCellsSection)
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-    }
-
-    
-    var isShowingLoadingIndicator: Bool {
-        return refreshControl?.isRefreshing == true
-    }
-    
-    func numberOfRenderedBeerCells() -> Int {
-        return tableView.numberOfRows(inSection: beerCellsSection)
-    }
-    
-    func beerCell(at row: Int) -> UITableViewCell? {
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: beerCellsSection)
-        return ds?.tableView(tableView, cellForRowAt: index)
-    }
-    
-    private var beerCellsSection: Int {
-        return 0
-    }
-}
-
 private extension BeerCell {
     var isShowingIbu: Bool {
         return !ibuLabel.isHidden
