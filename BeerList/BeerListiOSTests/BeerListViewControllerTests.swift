f745adf3fff6daf743f4f50f6d95f49f529c0769
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-24 23:15:33 -0300

    Move BeerListViewController to its own file in production target

--- BeerList/BeerListiOSTests/BeerListViewControllerTests.swift
+++ BeerList/BeerListiOSTests/BeerListViewControllerTests.swift
@@ -6,31 +6,8 @@
 //
 
 import XCTest
-import UIKit
+import BeerListiOS
 import BeerList
-
-final class BeerListViewController: UITableViewController {
-    private var loader: BeerListLoader?
-    
-    convenience init(loader: BeerListLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
 
 class BeerListViewControllerTests: XCTestCase {
 
