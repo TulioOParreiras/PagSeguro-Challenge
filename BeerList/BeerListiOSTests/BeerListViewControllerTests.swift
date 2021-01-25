8258d843b784e558bb525060770a04092e288a34
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-24 21:14:02 -0300

    Load beer list on view load

--- BeerList/BeerListiOSTests/BeerListViewControllerTests.swift
+++ BeerList/BeerListiOSTests/BeerListViewControllerTests.swift
@@ -7,9 +7,18 @@
 
 import XCTest
 
-final class BeerListViewController {
-    init(loader: BeerListViewControllerTests.LoaderSpy) {
+final class BeerListViewController: UIViewController {
+    private var loader: BeerListViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: BeerListViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
@@ -22,10 +31,23 @@ class BeerListViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_doesLoadBeerList() {
+        let loader = LoaderSpy()
+        let sut = BeerListViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 
 }
