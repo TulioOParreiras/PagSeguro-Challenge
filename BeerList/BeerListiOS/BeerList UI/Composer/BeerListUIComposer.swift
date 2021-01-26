d339cf3bf1e7faa1ed79bc7009d88c7b1fbefd64
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-25 21:43:08 -0300

    Move MainQueueDispatchDecorator to its own file

--- BeerList/BeerListiOS/BeerList UI/Composer/BeerListUIComposer.swift
+++ BeerList/BeerListiOS/BeerList UI/Composer/BeerListUIComposer.swift
@@ -36,38 +36,6 @@ private extension BeerListViewController {
     }
 }
 
-final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: BeerListLoader where T == BeerListLoader {
-    func load(completion: @escaping (BeerListLoader.LoadResult) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-extension MainQueueDispatchDecorator: BeerImageDataLoader where T == BeerImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (BeerImageDataLoader.Result) -> Void) -> BeerImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
 private final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
     
