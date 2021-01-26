0026ff6bc9aededcd97f7f719535b8acf0bf5afc
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-25 21:44:07 -0300

    Move WeakRefVirtualProxy to its own file

--- BeerList/BeerListiOS/BeerList UI/Composer/BeerListUIComposer.swift
+++ BeerList/BeerListiOS/BeerList UI/Composer/BeerListUIComposer.swift
@@ -36,26 +36,6 @@ private extension BeerListViewController {
     }
 }
 
-private final class WeakRefVirtualProxy<T: AnyObject> {
-    private weak var object: T?
-    
-    init(_ object: T) {
-        self.object = object
-    }
-}
-
-extension WeakRefVirtualProxy: BeerListLoadingView where T: BeerListLoadingView {
-    func display(_ viewModel: BeerListLoadingViewModel) {
-        object?.display(viewModel)
-    }
-}
-
-extension WeakRefVirtualProxy: BeerView where T: BeerView, T.Image == UIImage {
-    func display(_ model: BeerViewModel<UIImage>) {
-        object?.display(model)
-    }
-}
-
 private final class BeerListViewAdapter: BeerListView {
     private weak var controller: BeerListViewController?
     private let imageLoader: BeerImageDataLoader
---
+++ BeerList/BeerListiOS/BeerList UI/Composer/WeakRefVirtualProxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakRefVirtualProxy.swift
+//  BeerListiOS
+//
+//  Created by Tulio Parreiras on 25/01/21.
+//
+
+import UIKit
+import BeerList
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: BeerListLoadingView where T: BeerListLoadingView {
+    func display(_ viewModel: BeerListLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: BeerView where T: BeerView, T.Image == UIImage {
+    func display(_ model: BeerViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
