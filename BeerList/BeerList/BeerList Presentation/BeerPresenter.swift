69ae1b36fb2ae300bdf67fdc29bd0204dca4c75f
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 08:20:10 -0300

    Move BeerViewModel to its own file

--- BeerList/BeerList/BeerList Presentation/BeerPresenter.swift
+++ BeerList/BeerList/BeerList Presentation/BeerPresenter.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-public struct BeerViewModel<Image> {
-    public let name: String
-    public let ibuValue: Double?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
-    
-    public var ibu: String?  {
-        guard let ibu = ibuValue else { return nil }
-        return String(describing: ibu)
-    }
-    
-    public var hasIbu: Bool {
-        return ibuValue != nil
-    }
-}
-
 public protocol BeerView {
     associatedtype Image
     
