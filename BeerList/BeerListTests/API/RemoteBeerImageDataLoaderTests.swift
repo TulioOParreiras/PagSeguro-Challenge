6076d4308423524169c1bdde7f4e17c714582f32
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 08:57:07 -0300

    RemoteBeerImageDataLoader does not deliver result after cancelling task

--- BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
+++ BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
@@ -23,28 +23,45 @@ class RemoteBeerImageDataLoader {
         case invalidData
     }
     
-    private struct HTTPTaskWrapper: BeerImageDataLoaderTask {
-        let wrapped: HTTPClientTask
+    private final class HTTPClientTaskWrapper: BeerImageDataLoaderTask {
+        private var completion: ((BeerImageDataLoader.Result) -> Void)?
+
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (BeerImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: BeerImageDataLoader.Result) {
+            completion?(result)
+        }
         
         func cancel() {
-            wrapped.cancel()
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
         }
     }
     
     @discardableResult
     func loadImageData(from url: URL, completion: @escaping (BeerImageDataLoader.Result) -> Void) -> BeerImageDataLoaderTask {
-        return HTTPTaskWrapper(wrapped: client.get(from: url) { [weak self] result in
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
             switch result {
             case let .success((data, response)):
                 if response.statusCode == 200, !data.isEmpty {
-                    completion(.success(data))
+                    task.complete(with: .success(data))
                 } else {
-                    completion(.failure(Error.invalidData))
+                    task.complete(with: .failure(Error.invalidData))
                 }
-            case let .failure(error): completion(.failure(error))
+            case let .failure(error): task.complete(with: .failure(error))
             }
-        })
+        }
+        return task
     }
 }
 
@@ -138,6 +155,21 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.cancelledURLs, [url], "Expected cancelled URL request after task is cancelled")
     }
     
+    func test_loadImageDataFromURL_doesNotDeliverResultAfterCancellingTask() {
+        let (sut, client) = makeSUT()
+        let nonEmptyData = Data("non-empty data".utf8)
+
+        var received = [BeerImageDataLoader.Result]()
+        let task = sut.loadImageData(from: anyURL()) { received.append($0) }
+        task.cancel()
+        
+        client.complete(withStatusCode: 404, data: anyData())
+        client.complete(withStatusCode: 200, data: nonEmptyData)
+        client.complete(with: anyNSError())
+        
+        XCTAssertTrue(received.isEmpty, "Expected no received results after cancelling task")
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteBeerImageDataLoader, client: HTTPClientSpy) {
