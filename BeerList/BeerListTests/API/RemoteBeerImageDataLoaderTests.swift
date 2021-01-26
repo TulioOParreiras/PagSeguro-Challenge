7202bd8b212a6c8d0729ac90b94c5d837812bcdb
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 08:37:48 -0300

    RemoteBeerImageDataLoader delivers non-empty data on 200 http response

--- BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
+++ BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
@@ -26,7 +26,12 @@ class RemoteBeerImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (BeerImageDataLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success: completion(.failure(Error.invalidData))
+            case let .success(data, response):
+                if response.statusCode == 200, !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
             case let .failure(error): completion(.failure(error))
             }
         }
@@ -87,6 +92,15 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
         expect(sut, toCompleteWith: failure(.invalidData), when: {
             let emptyData = Data()
             client.complete(withStatusCode: 200, data: emptyData)
+        })
+    }
+    
+    func test_loadImageDataFromURL_deliversReceivedNonEmptyDataOn200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let nonEmptyData = Data("non-empty data".utf8)
+        
+        expect(sut, toCompleteWith: .success(nonEmptyData), when: {
+            client.complete(withStatusCode: 200, data: nonEmptyData)
         })
     }
 
