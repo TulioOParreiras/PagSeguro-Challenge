f033dd2d979cc41104063409bf059f21bf97f691
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 18:35:21 -0300

    Check snapshot images both for light and dark mode using the iPhone8 configuration

--- BeerList/BeerListiOSTests/BeerListSnapshotTests.swift
+++ BeerList/BeerListiOSTests/BeerListSnapshotTests.swift
@@ -16,7 +16,8 @@ class BeerListSnapshotTests: XCTestCase {
         
         sut.tableModel = emptyBeerList()
         
-        assert(snapshot: sut.snapshot(), named: "EMPTY_BEER_LIST")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_BEER_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_BEER_LIST_dark")
     }
     
     func test_beerListWithContent() {
@@ -24,7 +25,8 @@ class BeerListSnapshotTests: XCTestCase {
         
         sut.display(beerListWithContent())
         
-        assert(snapshot: sut.snapshot(), named: "BEER_LIST_WITH_CONTENT")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "BEER_LIST_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "BEER_LIST_WITH_CONTENT_dark")
     }
     
     func test_beerListWithErrorMessage() {
@@ -32,7 +34,8 @@ class BeerListSnapshotTests: XCTestCase {
         
         sut.display(.error(message: "This is a really long error message text to check how the view\nhandles multi-line\ntexts"))
         
-        assert(snapshot: sut.snapshot(), named: "BEER_LIST_WITH_ERROR_MESSAAGE")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "BEER_LIST_WITH_ERROR_MESSAAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "BEER_LIST_WITH_ERROR_MESSAAGE_dark")
     }
     
     func test_beerWithFailedImageLoading() {
@@ -40,7 +43,8 @@ class BeerListSnapshotTests: XCTestCase {
         
         sut.display(beerWithFailedImageLoading())
         
-        assert(snapshot: sut.snapshot(), named: "BEER_WITH_FAILED_IMAGE_LOADING")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "BEER_WITH_FAILED_IMAGE_LOADING_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "BEER_WITH_FAILED_IMAGE_LOADING_dark")
     }
     
     // MARK: - Helpers
@@ -125,10 +129,60 @@ class BeerListSnapshotTests: XCTestCase {
 }
 
 extension UIViewController {
+    func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
+        return SnapshotWindow(configuration: configuration, root: self).snapshot()
+    }
+}
+
+struct SnapshotConfiguration {
+    let size: CGSize
+    let safeAreaInsets: UIEdgeInsets
+    let layoutMargins: UIEdgeInsets
+    let traitCollection: UITraitCollection
+    
+    static func iPhone8(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+        return SnapshotConfiguration(
+            size: CGSize(width: 375, height: 667),
+            safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
+            layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+            traitCollection: UITraitCollection(traitsFrom: [
+                .init(forceTouchCapability: .available),
+                .init(layoutDirection: .leftToRight),
+                .init(preferredContentSizeCategory: .medium),
+                .init(userInterfaceIdiom: .phone),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(displayScale: 2),
+                .init(displayGamut: .P3),
+                .init(userInterfaceStyle: style)
+            ]))
+    }
+}
+
+private final class SnapshotWindow: UIWindow {
+    private var configuration: SnapshotConfiguration = .iPhone8(style: .light)
+    
+    convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+        self.init(frame: CGRect(origin: .zero, size: configuration.size))
+        self.configuration = configuration
+        self.layoutMargins = configuration.layoutMargins
+        self.rootViewController = root
+        self.isHidden = false
+        root.view.layoutMargins = configuration.layoutMargins
+    }
+    
+    override var safeAreaInsets: UIEdgeInsets {
+        return configuration.safeAreaInsets
+    }
+    
+    override var traitCollection: UITraitCollection {
+        return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
+    }
+    
     func snapshot() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        let renderer = UIGraphicsImageRenderer(bounds: bounds, format: .init(for: traitCollection))
         return renderer.image { action in
-            view.layer.render(in: action.cgContext)
+            layer.render(in: action.cgContext)
         }
     }
 }
