1d4699060519a6ee807d2486d5f9bc6e1117d94f
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 09:10:05 -0300

    Add RemoteBeerImageDataLoader end to end test to make sure both the app and server are respecting the contract

--- BeerList/BeerListAPIEndToEndTests/BeerListAPIEndToEndTests.swift
+++ BeerList/BeerListAPIEndToEndTests/BeerListAPIEndToEndTests.swift
@@ -20,11 +20,24 @@ class BeerListAPIEndToEndTests: XCTestCase {
         }
     }
     
+    func test_endToEndTestServerGETBeerImageDataResult_matchesFixedTestAccountData() {
+        switch getBeerImageDataResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful image data result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful image data result, got no result instead")
+        }
+    }
+    
     // MARK: - Helpers
     
     private func getBeersResult(file: StaticString = #file, line: UInt = #line) -> BeerListLoader.LoadResult {
         let testServerURL = URL(string: "https://api.punkapi.com/v2/beers")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteBeerListLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
@@ -38,6 +51,25 @@ class BeerListAPIEndToEndTests: XCTestCase {
         
         wait(for: [exp], timeout: 5.0)
         return expectedResult
+    }
+    
+    private func getBeerImageDataResult(file: StaticString = #file, line: UInt = #line) -> BeerImageDataLoader.Result? {
+        let testServerURL = URL(string: "https://images.punkapi.com/v2/keg.png")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteBeerImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: BeerImageDataLoader.Result?
+        _ = loader.loadImageData(from: testServerURL) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
     }
 
 }
