d0d02b90d77d3a0b723519dde6d679a3a26f6092
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 07:02:15 -0300

    Presenter does not display error on start loading

--- BeerList/BeerListiOSTests/Presentation/BeerListPresenterTests.swift
+++ BeerList/BeerListiOSTests/Presentation/BeerListPresenterTests.swift
@@ -7,9 +7,27 @@
 
 import XCTest
 
+struct BeerListErrorViewModel {
+    let message: String?
+    
+    static var noError: BeerListErrorViewModel {
+        return BeerListErrorViewModel(message: nil)
+    }
+}
+
+protocol BeerListErrorView {
+    func display(_ viewModel: BeerListErrorViewModel)
+}
+
 final class BeerListPresenter {
-    init(view: Any) {
-        
+    private let errorView: BeerListErrorView
+    
+    init(errorView: BeerListErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingBeerList() {
+        errorView.display(.noError)
     }
 }
 
@@ -21,18 +39,35 @@ class BeerListPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingBeerLIsst_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+
+        sut.didStartLoadingBeerList()
+
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: BeerListPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = BeerListPresenter(view: view)
+        let sut = BeerListPresenter(errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
 
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: BeerListErrorView {
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ viewModel: BeerListErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
+
     }
 
 }
