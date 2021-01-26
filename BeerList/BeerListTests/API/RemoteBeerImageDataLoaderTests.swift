7cc8ceb542f26f4f13cc723f772b79abbfb1aa90
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 08:35:46 -0300

    RemoteBeerImageDataLoader deliver invalid data error on non 200 http response

--- BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
+++ BeerList/BeerListTests/API/RemoteBeerImageDataLoaderTests.swift
@@ -19,11 +19,15 @@ class RemoteBeerImageDataLoader {
         self.client = client
     }
     
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
     func loadImageData(from url: URL, completion: @escaping (BeerImageDataLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
+            case .success: completion(.failure(Error.invalidData))
             case let .failure(error): completion(.failure(error))
-            default: break
             }
         }
     }
@@ -65,6 +69,18 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
         })
     }
     
+    func test_loadImageDataFromURL_deliversInvalidDataErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
+                client.complete(withStatusCode: code, data: anyData(), at: index)
+            })
+        }
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteBeerImageDataLoader, client: HTTPClientSpy) {
@@ -75,6 +91,14 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
 
+    private func anyData() -> Data {
+        return Data("any data".utf8)
+    }
+
+    private func failure(_ error: RemoteBeerImageDataLoader.Error) -> BeerImageDataLoader.Result {
+        return .failure(error)
+    }
+    
     private func expect(_ sut: RemoteBeerImageDataLoader, toCompleteWith expectedResult: BeerImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let url = URL(string: "https://a-given-url.com")!
         let exp = expectation(description: "Wait for load completion")
@@ -83,6 +107,10 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                
+            case let (.failure(receivedError as RemoteBeerImageDataLoader.Error), .failure(expectedError as RemoteBeerImageDataLoader.Error)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
                 
             case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
@@ -113,6 +141,16 @@ class RemoteBeerImageDataLoaderTests: XCTestCase {
         func complete(with error: Error, at index: Int = 0) {
             messages[index].completion(.failure(error))
 
+        }
+        
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            messages[index].completion(.success((data, response)))
         }
     }
 
