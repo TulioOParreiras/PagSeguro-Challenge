ed13b35e9ef888fa08a8994685b4e8f6c4c112b7
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-26 23:57:19 -0300

    Move BeerDetailsImageLoaderPresentationAdapter to its own file

--- BeerListApp/BeerListApp/Composer/BeerDetailsUIComposer.swift
+++ BeerListApp/BeerListApp/Composer/BeerDetailsUIComposer.swift
@@ -23,27 +23,3 @@ public final class BeerDetailsUIComposer {
     }
 }
 
-final class BeerDetailsImageLoaderPresentationAdapter<View: BeerDetailsView, Image>: BeerDetailsViewControllerDelegate where View.Image == Image {
-    private let imageLoader: BeerImageDataLoader
-    private let beer: Beer
-    var presenter: BeerDetailsPresenter<View, Image>?
-    
-    init(beer: Beer, imageLoader: BeerImageDataLoader) {
-        self.beer = beer
-        self.imageLoader = imageLoader
-    }
-    
-    func didRequestBeerImageLoad() {
-        let model = beer
-        presenter?.didStartLoadingImageData(for: model)
-        _ = imageLoader.loadImageData(from: beer.imageURL) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
-    }
-}
-
