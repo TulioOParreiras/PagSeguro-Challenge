75ed2172ad16c1f0cbfd3b5c8aa9bbd5ac4d5e59
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-27 09:53:34 -0300

    Move BeerImageDataLoaderWithFallbackComposite to its own file

--- BeerListApp/BeerListApp/Composer/BeerListUIComposer.swift
+++ BeerListApp/BeerListApp/Composer/BeerListUIComposer.swift
@@ -35,39 +35,6 @@ public final class BeerListUIComposer {
 
 }
 
-public class BeerImageDataLoaderWithFallbackComposite: BeerImageDataLoader {
-    private let primary: BeerImageDataLoader
-    private let fallback: BeerImageDataLoader
-
-    public init(primary: BeerImageDataLoader, fallback: BeerImageDataLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    private class TaskWrapper: BeerImageDataLoaderTask {
-        var wrapped: BeerImageDataLoaderTask?
-        
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-
-    public func loadImageData(from url: URL, completion: @escaping (BeerImageDataLoader.Result) -> Void) -> BeerImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primary.loadImageData(from: url) { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-                
-            case .failure:
-                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
-            }
-
-        }
-        return task
-    }
-}
-
 public final class BeerImageDataLoaderCacheDecorator: BeerImageDataLoader {
     private let decoratee: BeerImageDataLoader
     private let cache: BeerImageDataCache
