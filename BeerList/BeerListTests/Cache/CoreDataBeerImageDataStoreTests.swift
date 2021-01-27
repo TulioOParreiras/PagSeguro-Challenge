71212c6b44916caabbacc57700b7edcf838e80c6
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-27 09:33:34 -0300

    Move `CoreDataImageStore` implementation of the `BeerImageDataStore` protocol to production

--- BeerList/BeerListTests/Cache/CoreDataBeerImageDataStoreTests.swift
+++ BeerList/BeerListTests/Cache/CoreDataBeerImageDataStoreTests.swift
@@ -8,29 +8,6 @@
 import XCTest
 @testable import BeerList
 
-extension CoreDataBeerStore: BeerImageDataStore {
-    
-    public func insert(_ data: Data, for url: URL, completion: @escaping (BeerImageDataStore.InsertionResult) -> Void) {
-        perform { context in
-            completion(Result {
-                let managedBeerImage = try ManagedBeerImage.newUniqueInstance(in: context)
-                managedBeerImage.data = data
-                managedBeerImage.url = url
-                try context.save()
-            })
-        }
-    }
-    
-    public func retrieve(dataForURL url: URL, completion: @escaping (BeerImageDataStore.RetrievalResult) -> Void) {
-        perform { context in
-            completion(Result {
-                try ManagedBeerImage.first(with: url, in: context)?.data
-            })
-        }
-    }
-    
-}
-
 class CoreDataBeerImageDataStoreTests: XCTestCase {
     
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
