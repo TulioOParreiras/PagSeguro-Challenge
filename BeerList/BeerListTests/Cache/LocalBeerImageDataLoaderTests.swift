6c25d44a4599a4dc5fe65b9cd7d6807d245ebac8
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-27 08:00:14 -0300

    `LocalBeerImageDataLoader` does not message store upon creation

---
+++ BeerList/BeerListTests/Cache/LocalBeerImageDataLoaderTests.swift
@@ -0,0 +1,40 @@
+//
+//  LocalBeerImageDataLoaderTests.swift
+//  BeerListTests
+//
+//  Created by Tulio Parreiras on 27/01/21.
+//
+
+import XCTest
+import BeerList
+
+final class LocalFeedImageDataLoader {
+    init(store: Any) {
+        
+    }
+}
+
+class LocalBeerImageDataLoaderTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private class FeedStoreSpy {
+        let receivedMessages = [Any]()
+    }
+
+
+}
