3138b819dc69259da08015dd9d4ff4c56a2d2e44
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-27 08:30:09 -0300

    Extract cache beer image data use case into the new `CacheBeerImageDataUseCaseTests`

---
+++ BeerList/BeerListTests/Cache/CacheBeerImageDataUseCaseTests.swift
@@ -0,0 +1,40 @@
+//
+//  CacheBeerImageDataUseCaseTests.swift
+//  BeerListTests
+//
+//  Created by Tulio Parreiras on 27/01/21.
+//
+
+import XCTest
+import BeerList
+
+class CacheBeerImageDataUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        let data = anyData()
+        
+        sut.save(data, for: url) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalBeerImageDataLoader, store: BeerImageDataStoreSpy) {
+        let store = BeerImageDataStoreSpy()
+        let sut = LocalBeerImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+
+
+}
