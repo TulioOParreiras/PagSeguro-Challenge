c2d38bddb86cbe995a912ea813b097f287bab4d4
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-01-27 08:33:05 -0300

    `LocalBeerImageDataLoader.saveImageDataForURL` requests image data insertion into store

--- BeerList/BeerListTests/Cache/Helpers/BeerImageDataStoreSpy.swift
+++ BeerList/BeerListTests/Cache/Helpers/BeerImageDataStoreSpy.swift
@@ -14,10 +14,13 @@ class BeerImageDataStoreSpy: BeerImageDataStore {
         case retrieve(dataFor: URL)
     }
     private var retrievalCompletions = [(BeerImageDataStore.RetrievalResult) -> Void]()
+    private var insertionCompletions = [(BeerImageDataStore.InsertionResult) -> Void]()
+
     private(set) var receivedMessages = [Message]()
     
     func insert(_ data: Data, for url: URL, completion: @escaping (BeerImageDataStore.InsertionResult) -> Void) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
 
     func retrieve(dataForURL url: URL, completion: @escaping (BeerImageDataStore.RetrievalResult) -> Void) {
@@ -31,5 +34,9 @@ class BeerImageDataStoreSpy: BeerImageDataStore {
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
